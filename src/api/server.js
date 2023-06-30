0f3efa4b6bebf485ef2143c6310d0e0171826580
n0xgg04 <dulieu.vblc@gmail.com>
2023-07-01 02:14:18 +0700

    For vercel

--- src/api/server.js
+++ src/api/server.js
@@ -4,6 +4,7 @@ import router from '../routes/index.js';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import {v4} from 'uuid';
 
 const app = express();
 app.use(cors({
@@ -13,6 +14,12 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 dotenv.config();
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
 app.use('/', router);
 
