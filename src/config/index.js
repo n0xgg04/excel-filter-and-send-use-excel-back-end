cc8ea5cd24eafe8732bf82a1783ed2f63eb81851
n0xgg04 <36061508+n0xgg04@users.noreply.github.com>
2023-07-10 15:19:53 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 const info = {
     name: '',
-    CLIENT_ID: '554506097930-iehoj8odb9kakmfrih8u4vl7eqv0ttfq.apps.googleusercontent.com',
-    CLIENT_SECRET: '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',
-    REDIRECT_URI: 'https://developers.google.com/oauthplayground',
-    MY_EMAIL: 'dulieu.vblc@gmail.com',
-    REFRESH_TOKEN: "1//04r8rwqDyXONjCgYIARAAGAQSNwF-L9IrgXHWhScKu59hg9MVLbdj36KrvnAgxlA_xwmgzcGjTF4Z-Tlq9SQefrg0Wl1X6CIjJbg"
+    CLIENT_ID: '',
+    CLIENT_SECRET: '',
+    REDIRECT_URI: '',
+    MY_EMAIL: '',
+    REFRESH_TOKEN: ""
 };
 
 
